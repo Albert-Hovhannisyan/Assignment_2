1b77b40194693a74eaf02c37cf41226fa112a45d
Albert Hovhannisyan <20albert01@gmail.com>
2022-03-30 23:04:44 +0400

    Update error-handler.middleware.js

--- common/middlewares/error-handler.middleware.js
+++ common/middlewares/error-handler.middleware.js
@@ -1,14 +1,9 @@
 const NotFoundError = require('../errors/not-found.error');
-const BadRequestError = require('../errors/bad-request.error')
 
 module.exports = (err, req, res, next) => {
 
     if (err instanceof NotFoundError) {
         return res.status(404).send(err.message);
-    }
-
-    if (err instanceof BadRequestError) {
-        return res.status(400).send(err.message);
     }
 
     res.status(500).send('No cities found!');
