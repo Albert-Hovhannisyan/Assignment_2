4146b55641725681419679bdae23905372cd123f
Albert Hovhannisyan <20albert01@gmail.com>
2022-03-30 23:04:19 +0400

    Update cities.controller.js

--- cities/cities.controller.js
+++ cities/cities.controller.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const citiesService = require('./cities.service');
 const asyncHandler = require('express-async-handler');
-const BadRequestError = require('../common/errors/bad-request.error');
 
 const route = express.Router();
 
 route.get('/:zipCode/', asyncHandler(async (req, res) => {
     const zipcode = req.params['zipCode'];
-
-    if(isNaN(zipcode) || zipcode <= 0){
-        throw new BadRequestError('Please enter a valid zipcode using only natural numbers!');
-    }
-
     const result = await citiesService.getCityByZipCode(zipcode);
     res.status(200).send(result);
 }))
